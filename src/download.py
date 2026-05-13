347da39845f15561d719998e5d20420ce0364552
Icemberg1604 <marcosarizanieves@gmail.com>
2026-05-13 07:38:47 +0200

    Modificación del download para que se ejecute todo desde la terminal.

--- src/download.py
+++ src/download.py
@@ -2,40 +2,58 @@ import urllib.request
 import json
 import os
 from typing import Any
+import sys
+
 BASE_URL = "https://klotski.pauek.dev"
 
 def get_puzzles() -> Any:
+
     url = f"{BASE_URL}/api/puzzles"
+
     with urllib.request.urlopen(url) as response:
         data = response.read()
         return json.loads(data)
 
 def download_puzzle(puzzle_id: str, save_folder="puzzles") -> None:
+
     url = f"{BASE_URL}/api/puzzles/{puzzle_id}"
+
     with urllib.request.urlopen(url) as response:
         puzzle_data = json.loads(response.read())
+
     os.makedirs(save_folder, exist_ok=True)
-    name = input("Escribe el nombre:\n")
+    
+    if len(sys.argv) == 4:
+        name = sys.argv[3]
+
+    else:
+        name = puzzle_id
+
     file_path = os.path.join(save_folder, f"{name}.json")
+
     with open(file_path, "w") as f:
         json.dump(puzzle_data, f, indent=2)
         
-    print(f"Puzzle {puzzle_id} descargado con éxito.")
+    print(f"Puzzle {puzzle_id} descargado con éxito. \n")
 
 
 def main()-> None:
-    instruction = input("Selecciona que quieres hacer: download o get: \n")
+    
+    instruction = sys.argv[1]
     if instruction == "download":
-        puzzle_id = input("Escribe la id del puzzle: \n")
+        try:
+            puzzle_id = sys.argv[2]
+        except:
+            raise KeyError("No se ha puesto una id")
+        
         download_puzzle(puzzle_id=puzzle_id)
     
     elif instruction == "get":
-
         puzzle_list = get_puzzles()
         print(puzzle_list)
 
     else:
-        raise NotImplemented("No existe este comando\n")
+        raise NotImplemented("No existe este comando")
 
     
 if __name__ == '__main__':
