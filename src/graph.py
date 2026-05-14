ebdb563e4d3d4dd6d946c3cc2a10db37d0ce0aae
Icemberg1604 <marcosarizanieves@gmail.com>
2026-05-14 22:18:43 +0200

    Version buena de graph (no final)

--- src/graph.py
+++ src/graph.py
@@ -1,27 +1,59 @@
 import graph_tool.all as gt
 import sys
 import logic as lg
-from typing import TypeAlias
+from typing import TypeAlias, Any
 from puzzle import Puzzle, State
-from pathlib import Path
 from collections import deque
-from typing import Any
 import json
 
-StateKey: TypeAlias = tuple[tuple[int, int],...]
+Coord: TypeAlias = tuple[int, int]
+StateKey: TypeAlias = tuple[Coord,...]
 
 
-def state_key(puzzle: Puzzle, state_data: State | str) -> StateKey:
-    if isinstance(state_data, str):
-        positions = json.loads(state_data)
-        return tuple(tuple(p) for p in positions)
+def equiv_shapes(puzzle: Puzzle) -> tuple[tuple[int,...],...]:
+    
+    goal_pieces: set[int] = {goal[0] for goal in puzzle.goals}
+    dict_equiv_shapes: dict[StateKey, list[int]] = {}
+
+    for index, piece in enumerate(puzzle.pieces):
+        if index not in goal_pieces:
+            shape = piece.coords
+            if shape not in dict_equiv_shapes:
+                dict_equiv_shapes[shape] =  []
+            dict_equiv_shapes[shape].append(index)
+        
+    return tuple(tuple(indexes) for indexes in dict_equiv_shapes.values() if len(indexes) > 1)
+
+def get_canonical_position(state_data: State, identical_pieces: tuple[tuple[int,...],...]) -> StateKey:
+    pos_l = list(state_data.positions)
+    for group in identical_pieces:
+        new_coords = sorted([state_data.positions[piece_index] for piece_index in group ])
+        for index, coord in zip(group, new_coords):
+            pos_l[index] = coord
+
+    return tuple(pos_l)
+
+def state_key(puzzle: Puzzle, state: State | str) -> StateKey:
+    """
+    Adapting function for 3D_view.py. 
+    Cleans the entry data and uses the calculations of get_canonical_position to return the StateKey.
+    """
+    identical_pieces = equiv_shapes(puzzle)
+
+    if isinstance(state, str):
+        parsed_positions = tuple(tuple(p) for p in json.loads(state))
+        clean_state = State(parsed_positions)
+        
     else:
-        return state_data.positions
+        clean_state = state
+
+    return get_canonical_position(clean_state, identical_pieces)
+
 
 def create_empty_graph() -> gt.Graph:
     g = gt.Graph(directed=False)
     
-    # v_property
+    # v_properties
     v_is_start = g.new_vertex_property("bool")
     v_is_goal = g.new_vertex_property("bool")
     v_state = g.new_vertex_property("string")
@@ -29,7 +61,7 @@ def create_empty_graph() -> gt.Graph:
     # e_property
     e_move = g.new_edge_property("string")
     
-    # Guardamos las propiedades en el grafo
+    # We save this properties in the graph, so we can access to them later
     g.vertex_properties["is_start"] = v_is_start
     g.vertex_properties["is_goal"] = v_is_goal
     g.vertex_properties["state"] = v_state
@@ -38,60 +70,81 @@ def create_empty_graph() -> gt.Graph:
     return g
 
 def constructive_bfs(puzzle: Puzzle) -> gt.Graph:
-    g = create_empty_graph()
-    visited: dict[State, Any] = {}
-    
-    # CORRECCIÓN 1: Inicializamos la cola vacía correctamente
+
+    g = create_empty_graph() #creates a graph with ids for its state, and if it is a goal vertex or not.
+
+    visited: dict[StateKey, Any] = {}  #type: ignore
     queue: deque[State] = deque()
+    same_shape_pieces = equiv_shapes(puzzle=puzzle)
 
-    def state_eval(state: State) -> int:
-        if state not in visited:
+    start_canonical = get_canonical_position(puzzle.start, same_shape_pieces)
+    v1 = g.add_vertex()
+    visited[start_canonical] = v1
+    queue.append(puzzle.start)
 
+    g.vp["state"][v1] = puzzle.start.to_json()
+    g.vp["is_start"][v1] = True
+    g.vp["is_goal"][v1] = lg.is_goal(puzzle, puzzle.start)
+
+
+
+
+    def evaluate(state: State, visited: dict[StateKey, Any], same_shape) -> Any:
+        canonical_state = get_canonical_position(state, same_shape)
+        if canonical_state not in visited:
             v = g.add_vertex()
-            visited[state] = v
+            visited[canonical_state] = v
 
             g.vp["is_goal"][v] = lg.is_goal(puzzle, state)
-            g.vp["is_goal"][v] = state.to_json()
-
+            g.vp["state"][v] = state.to_json()
+            
             queue.append(state)
 
-        return visited[state]
-
-    v_start_id = state_eval(puzzle.start)
-    g.vp["is_start"][g.vertex(v_start_id)] = True
-
-    # BFS
+        return visited[canonical_state]
+    
     while queue:
-        current_state = queue.popleft()
-        v_curr = visited[current_state]
 
-        for move in lg.possible_moves(puzzle, current_state):
-            new_state = lg.apply_move(puzzle, current_state, move)
-            v_next = state_eval(new_state)
+        v_state = queue.popleft()
+        possible_moves = lg.possible_moves(puzzle, v_state)
+        v_curr_canonical = get_canonical_position(v_state, same_shape_pieces)
+        v_curr_id = visited[v_curr_canonical]
 
-            # Si la conexión no existe (para evitar aristas duplicadas al volver atrás)
-            if not g.edge(v_curr, v_next):
-                e = g.add_edge(v_curr, v_next)
+        for move in possible_moves:
+            u_state = lg.apply_move(puzzle, v_state, move)
+            v_next_id = evaluate(u_state, visited, same_shape_pieces)
+
+            if not g.edge(v_curr_id, v_next_id):
+                e = g.add_edge(v_curr_id, v_next_id)
                 p_idx, direction, _ = move
-                g.ep["move"][e] = f"{p_idx},{direction}" 
+                g.ep["move"][e] = f"{p_idx},{direction}"
 
-    return g
+    return g 
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        print("Use: python src/graph.py puzzles/nombre_puzzle.json")
-        sys.exit(1)
+    try:
+        json_path = sys.argv[1]
 
-    json_path = Path(sys.argv[1])
-    puzzle = Puzzle.from_json(json_path.read_text())
-    
+    except IndexError:
+        raise Exception("No se ha puesto la ruta del archivo JSON. Uso: python graph.py puzzles/nombre_puzzle.json")
+
+    with open(json_path, 'r', encoding='utf-8') as file:
+        json_text = file.read()
+        
+    puzzle = Puzzle.from_json(json_text)
+    print(f"Building graph for {json_path}...")
     graph = constructive_bfs(puzzle) 
     
-    out_path = json_path.with_suffix('.graphml')
-    graph.save(str(out_path))
-    
-    print(f"Grafo terminado: {graph.num_vertices()} estados.")
-    print(f"Guardado exitosamente en: {out_path}")
+    graph.gp["puzzle"] = graph.new_graph_property("string")
+    graph.gp["puzzle"] = json_text
+
+    if json_path.endswith(".json"):
+        out_path = json_path[:-5] + ".graphml"  # Le quitamos los últimos 5 caracteres (".json")
+
+    else:
+        out_path = json_path + ".graphml"       # Por si acaso le pasan un archivo sin extensión
+
+    graph.save(out_path)
+    print(f"Grafo guardado exitosamente en: {out_path}")
 
 if __name__ == "__main__":
     main()
